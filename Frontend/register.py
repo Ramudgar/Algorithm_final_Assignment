77783d2e6b49bfd08ee714e82fd440bfd8f4bd64
Ramudgar <ramudgary52@gmail.com>
2021-03-03 01:41:43 +0545

    6th commit- Updated register page

--- Frontend/register.py
+++ Frontend/register.py
@@ -1,7 +1,11 @@
-# use doc string to make this code attractive this task
+# use doc string to make this code attractive
 from tkinter import *
 from tkinter import ttk, messagebox
 from PIL import ImageTk  # pip install pillow
+
+import Frontend.login
+#import backend.dbconnection
+#import Models.user
 
 
 class Register():
@@ -11,14 +15,13 @@ class Register():
         self.root.geometry("1350x700+0+0")
         self.root.config(bg="white")
 
-        # self.root.resizable((False,False))
-
-        ### Background image"""
+        #self.db = backend.dbconnection.DBConnect()
+        ### Background image
         self.bg = ImageTk.PhotoImage(file="images\maxresdefault.jpg")
         bg = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
         # === Left Image====
-        self.left = ImageTk.PhotoImage(file="images/sensors.jpg")  # if .png image is available
+        self.left = ImageTk.PhotoImage(file="images/sensors.jpg")
         left = Label(self.root, image=self.left).place(x=80, y=100, width=400, height=500)
 
         # ======Register frame======
@@ -29,11 +32,13 @@ class Register():
         # self.var_fname=StringVar()
         title = Label(frame1, text="REGISTER HERE", font=("times new roman", 20, "bold"), bg="white", fg="Green").place(
             x=50, y=30)
+
         # ======First Name===Entry field=======
         f_name = Label(frame1, text="First Name", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
             x=50, y=100)
         self.txt_fname = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_fname.place(x=50, y=130, width=250)
+
         # ==== Last Name===========
         l_name = Label(frame1, text="Last Name", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
             x=370, y=100)
@@ -55,10 +60,12 @@ class Register():
         # ========Security question=====
         question = Label(frame1, text="Security Question", font=("times new roman", 13, "bold"), bg="white",
                          fg="Black").place(x=50, y=240)
+
         # Using combo box=====
         self.cmb_question = ttk.Combobox(frame1, font=("times new roman", 15), state="readonly", justify=CENTER)
         self.cmb_question["values"] = ("select", "Your first pet name", "Your birt place", "Your best friend Name")
         self.cmb_question.place(x=50, y=270, width=250)
+
         # to set "select" as defaul in combo box
         self.cmb_question.current(0)
         # =======Answer============
@@ -66,16 +73,19 @@ class Register():
                                                                                                                   y=240)
         self.txt_answer = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_answer.place(x=370, y=270, width=250)
+
         # ====Password=======
         pasword = Label(frame1, text="Password", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
             x=50, y=310)
         self.txt_password = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_password.place(x=50, y=340, width=250)
+
         # ======Confirm password============
         password_confirm = Label(frame1, text="Confirm Password", font=("times new roman", 15, "bold"), bg="white",
                                  fg="Black").place(x=370, y=310)
         self.txt_confirm = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_confirm.place(x=370, y=340, width=250)
+
         # -------  check box-------
 
         self.var_chk = IntVar()
@@ -88,8 +98,11 @@ class Register():
                                    fg="blue", bd="5", cursor="hand2", command=self.register_data).place(x=50, y=410,
                                                                                                         width=250)
 
-        self.login_btn = Button(frame1, text="Sign In", font=("times new roman", 14, "bold"), bg="white", fg="blue",
-                                bd="5", cursor="hand2").place(x=370, y=410, width=250)
+
+        lbl_signup = Label(frame1, text="Sign In", font=("times new roman", 14, "bold"), bg="white", fg="blue",
+                             bd="5", cursor="hand2")
+        lbl_signup.place(x=370, y=410)
+        lbl_signup.bind('<Button-1>', self.lbl_signin_click)
 
     def register_data(self):
         if self.txt_fname.get() == "" or self.txt_Contact_no.get() == "" or self.txt_email.get() == "" or self.cmb_question.get() == "select" or self.txt_answer.get() == "" or self.txt_password.get() == "" or self.txt_confirm.get() == "":
@@ -101,11 +114,11 @@ class Register():
         elif self.var_chk.get() == 0:
             messagebox.showerror("Error", "Comobox is uncheckez, plz check it", parent=self.root)
 
-
         else:
             messagebox.showinfo("success", "Register sucessful", parent=self.root)
 
 
-root = Tk()
-obj = Register(root)
-root.mainloop()
+    def lbl_signin_click(self, event):
+        tk = Toplevel()
+        Frontend.login.Login_window(tk)
+
--- test.py
+++ test.py
@@ -1,86 +1,111 @@
+# use doc string to make this code attractive this task
 from tkinter import *
-from PIL import Image, ImageTk, ImageDraw
-from datetime import *
-import time
-from math import *
+from tkinter import ttk, messagebox
+from PIL import ImageTk  # pip install pillow
 
 
-class Login_window:
+class Register():
     def __init__(self, root):
         self.root = root
-        self.root.title("Login window")
+        self.root.title("Registration window")
         self.root.geometry("1350x700+0+0")
-        self.root.config(bg="#021e2f")
+        self.root.config(bg="white")
 
-        title = Label(self.root, text="Login System for Hotel Management System", font=("poopins", 20, "bold"),
-                      bg="#04444a", fg="white").place(x=0, y=0)
+        # self.root.resizable((False,False))
 
-        left_lbl = Label(self.root, bg="#021e2f", bd=0)
-        left_lbl.place(x=0, y=0, relheight=1, width=600)
+        ### Background image"""
+        self.bg = ImageTk.PhotoImage(file="images\maxresdefault.jpg")
+        bg = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
-        right_lbl = Label(self.root, bg="#031F3c", bd=0)
-        right_lbl.place(x=600, y=0, relheight=1, relwidth=1)
+        # === Left Image====
+        self.left = ImageTk.PhotoImage(file="images/sensors.jpg")  # if .png image is available
+        left = Label(self.root, image=self.left).place(x=80, y=100, width=400, height=500)
 
-        # ======= Frame====================
-        login_frame = Frame(self.root, bg="white")
-        login_frame.place(x=250, y=100, width=800, height=500)
+        # ======Register frame======
+        frame1 = Frame(self.root, bg="white")
+        frame1.place(x=480, y=100, width=700, height=500)
 
-        title = Label(login_frame, text="LOGIN HERE", font=("poopins", 30, "bold"), bg="white", fg="#08A3D2").place(
-            x=300, y=50)
+        # =============String Var======
+        # self.var_fname=StringVar()
+        title = Label(frame1, text="REGISTER HERE", font=("times new roman", 20, "bold"), bg="white", fg="Green").place(
+            x=50, y=30)
+        # ======First Name===Entry field=======
+        f_name = Label(frame1, text="First Name", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
+            x=50, y=100)
+        self.txt_fname = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_fname.place(x=50, y=130, width=250)
+        # ==== Last Name===========
+        l_name = Label(frame1, text="Last Name", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
+            x=370, y=100)
+        self.txt_lname = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_lname.place(x=370, y=130, width=250)
 
-        email = Label(login_frame, text="Email Address", font=("poopins", 18, "bold"), bg="white", fg="black").place(
-            x=250, y=150)
-        self.txt_email = Entry(login_frame, font=("Times new roman", 15), bg="Lightgray")
-        self.txt_email.place(x=250, y=180, width=350, height=35)
+        # ======Contact no============
+        Contact_no = Label(frame1, text="Contact No:", font=("times new roman", 15, "bold"), bg="white",
+                           fg="Black").place(x=50, y=170)
+        self.txt_Contact_no = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_Contact_no.place(x=50, y=200, width=250)
 
-        pasw = Label(login_frame, text="Password", font=("poopins", 18, "bold"), bg="white", fg="black").place(x=250,
-                                                                                                               y=250)
-        self.txt_pasw = Entry(login_frame, font=("Times new roman", 15), bg="Lightgray")
-        self.txt_pasw.place(x=250, y=280, width=350, height=35)
+        # =======Email============
+        email = Label(frame1, text="Email ID", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
+            x=370, y=170)
+        self.txt_email = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_email.place(x=370, y=200, width=250)
 
-        btn_reg = Button(login_frame, text="Register new Account", font=("Times new roman", 14, "bold"), bg="white",
-                         bd=1, fg="red", cursor="hand2").place(x=250, y=340)
-        btn_log = Button(login_frame, text="LOGIN", font=("POOPINS", 20, "bold"), bg="white", fg="green",
-                         cursor="hand2").place(x=250, y=390, width=180, height=40)
+        # ========Security question=====
+        question = Label(frame1, text="Security Question", font=("times new roman", 13, "bold"), bg="white",
+                         fg="Black").place(x=50, y=240)
+        # Using combo box=====
+        self.cmb_question = ttk.Combobox(frame1, font=("times new roman", 15), state="readonly", justify=CENTER)
+        self.cmb_question["values"] = ("select", "Your first pet name", "Your birt place", "Your best friend Name")
+        self.cmb_question.place(x=50, y=270, width=250)
+        # to set "select" as defaul in combo box
+        self.cmb_question.current(0)
+        # =======Answer============
+        answer = Label(frame1, text="Answer", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(x=370,
+                                                                                                                  y=240)
+        self.txt_answer = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_answer.place(x=370, y=270, width=250)
+        # ====Password=======
+        pasword = Label(frame1, text="Password", font=("times new roman", 15, "bold"), bg="white", fg="Black").place(
+            x=50, y=310)
+        self.txt_password = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_password.place(x=50, y=340, width=250)
+        # ======Confirm password============
+        password_confirm = Label(frame1, text="Confirm Password", font=("times new roman", 15, "bold"), bg="white",
+                                 fg="Black").place(x=370, y=310)
+        self.txt_confirm = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_confirm.place(x=370, y=340, width=250)
+        # -------  check box-------
 
-        # ==========clock============================
-        self.lbl = Label(self.root, text="\nWebcode clock", font=("Book Antiqua", 25, "bold"), compound=BOTTOM,
-                         bg="#081923", fg="White", bd=0)
-        self.lbl.place(x=90, y=120, height=450, width=350)
+        self.var_chk = IntVar()
+        chk_box = Checkbutton(frame1, text="I Agree The Terms And Condition", variable=self.var_chk, onvalue=1,
+                              offvalue=0, bg='white', font=("times new roman", 12)).place(x=50, y=370)
 
-        self.working()
+        # ----set button------
 
-    def clock_image(self, hr, min_, sec_):
-        clock = Image.new("RGB", (400, 400), (8, 25, 35))
-        draw = ImageDraw.Draw(clock)
-        #  for clock Image=====
-        bg = Image.open("images/clock1.jpg")
-        bg = bg.resize((300, 300), Image.ANTIALIAS)
-        clock.paste(bg, (50, 50))
-        # ===for Hour line Image===
-        origin = 200, 200
-        draw.line((origin, 200 + 50 * sin(radians(hr)), 200 - 50 * cos(radians(hr))), fill="black", width=4)
-        # ===for minute line Image===
-        draw.line((origin, 200 + 80 * sin(radians(min_)), 200 - 80 * cos(radians(min_))), fill="blue", width=3)
-        # ===for Second line Image===
-        draw.line((origin, 200 + 100 * sin(radians(sec_)), 200 - 100 * cos(radians(sec_))), fill="green", width=2)
-        draw.ellipse((190, 190, 210, 210), fill="black")
-        clock.save("clock_new.png")
+        self.register_btn = Button(frame1, text="Register Now", font=("times new roman", 14, "bold"), bg="white",
+                                   fg="blue", bd="5", cursor="hand2", command=self.register_data).place(x=50, y=410,
+                                                                                                        width=250)
 
-    def working(self):
-        h = datetime.now().time().hour
-        m = datetime.now().time().minute
-        s = datetime.now().time().second
+        self.login_btn = Button(frame1, text="Sign In", font=("times new roman", 14, "bold"), bg="white", fg="blue",
+                                bd="5", cursor="hand2").place(x=370, y=410, width=250)
 
-        hr = (h / 12) * 360
-        min_ = (m / 60) * 360
-        sec_ = (s / 60) * 360
-        self.clock_image(hr, min_, sec_)
-        self.img = ImageTk.PhotoImage(file="clock_new.png")
-        self.lbl.config(image=self.img)
-        self.lbl.after(190, self.working)
+    def register_data(self):
+        if self.txt_fname.get() == "" or self.txt_Contact_no.get() == "" or self.txt_email.get() == "" or self.cmb_question.get() == "select" or self.txt_answer.get() == "" or self.txt_password.get() == "" or self.txt_confirm.get() == "":
+            messagebox.showerror("Error", "All fields are Required", parent=self.root)
+
+        elif self.txt_password.get() != self.txt_confirm.get():
+            messagebox.showerror("Error", "Password and confirm password should be same", parent=self.root)
+
+        elif self.var_chk.get() == 0:
+            messagebox.showerror("Error", "Comobox is uncheckez, plz check it", parent=self.root)
+
+
+        else:
+            messagebox.showinfo("success", "Register sucessful", parent=self.root)
 
 
 root = Tk()
-obj = Login_window(root)
+obj = Register(root)
 root.mainloop()
