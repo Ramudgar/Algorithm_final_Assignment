a65047a2be2218036953a9ed6e78a1e7d8af97b2
Ramudgar <ramudgary52@gmail.com>
2021-03-03 00:13:23 +0545

    4th commit- main was created

---
+++ main.py
@@ -0,0 +1,12 @@
+import Frontend.login
+from tkinter import *
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tk=Tk()
+    Frontend.login.Login_window(tk)
+    tk.mainloop()
+
+
+
